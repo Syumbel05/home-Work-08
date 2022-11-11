39655c801caed974d3d434c4cca51309ad70aa17
syumbel <syu.kashapova@mail.ru>
2022-11-11 15:00:11 +0500

    добавила код 4 задания

---
+++ hw04/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+
+*/
+
+
+ int [, ,] array=new int[2,2,2];
+ List<int> nums= new List<int>();
+ for (int i=0;i<90;i++)
+ nums.Add(i+10);
+ for (int i=0;i<array.GetLength(0);i++)
+ {
+
+    for (int j=0;j<array.GetLength(1);j++)
+    {
+        for (int k=0;k<array.GetLength(2);k++)
+        {
+            if(nums.Count==0)
+            array[i,j,k]=0;
+            else
+            {
+               int temp=new Random().Next(nums.Count);
+               array[i,j,k]=nums[temp];
+               nums.RemoveAt(temp);
+            }
+        }
+    }
+ }
+ for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k ++)
+            {
+                Console.Write($"{array[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+    }
